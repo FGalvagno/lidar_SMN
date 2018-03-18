cdf304c0f2a25ae202fa0465f8f99bb1213ba095
Leonardo Mingari <lmingari@gmail.com>
2018-03-18 18:05:43 +0000

    First operational version

---
+++ make_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+version=2.1
+RUNPATH=/home/lmingari/lidar_v$version
+OUTPATH=/home/lmingari/salida
+
+$RUNPATH/main.py aeroparque > $OUTPATH/aeroparque/last.log 2>&1
+#$RUNPATH/main.py bariloche > $OUTPATH/bariloche/last.log 2>&1
+$RUNPATH/main.py comodoro > $OUTPATH/comodoro/last.log 2>&1
+#$RUNPATH/main.py cordoba > $OUTPATH/cordoba/last.log 2>&1
+$RUNPATH/main.py neuquen > $OUTPATH/neuquen/last.log 2>&1
+#$RUNPATH/main.py gallegos > $OUTPATH/gallegos/last.log 2>&1
+#$RUNPATH/main.py tucuman > $OUTPATH/tucuman/last.log 2>&1
+$RUNPATH/main.py parenas > $OUTPATH/parenas/last.log 2>&1
+#$RUNPATH/main.py vmartelli > $OUTPATH/vmartelli/last.log 2>&1
+
